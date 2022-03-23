297b58c37f0539478ebf3e8ec9e6414f9608751c
El-Litios <carlosguzmanrojas00@gmail.com>
2022-03-22 23:46:05 -0300

    init

---
+++ app/Models/User_Model.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class User_Model extends Model
+{
+    protected $table = 'user';
+    protected $allowedFields = ['name', 'email', 'password'];
+    protected $updatedField = 'updated_at';
+    
+    protected $beforeInsert = ['beforeInsert'];
+    protected $beforeUpdate = ['beforeUpdate'];
+
+    //CALLBACKS FUNCTIONS
+    protected function beforeInsert(array $data): array
+    {
+        return $this->getUpdatedDataWithHashPass($data);
+    }
+
+    protected function beforeUpdate(array $data): array
+    {
+        return $this->getUpdatedDataWithHashPass($data);
+    }
+
+
+    //FUNCTIONS
+    private function getUpdatedDataWithHashPass(array $data)
+    {
+        if(isset($data['data']['password']))
+        {
+            $textPlainedPassword = $data['data']['password'];
+            $data['data']['password'] = password_hash($textPlainedPassword, PASSWORD_BCRYPT);
+        }
+
+        return $data;
+    }
+
+    public function findUserByEmail(string $email)
+    {
+        $user = $this->asArray()->where(['email' => $email])->first();
+
+        if(!$user)
+        {
+            throw new \Exception('User does not exist for specific email');
+        }
+
+        return $user;
+    }
+
+}
