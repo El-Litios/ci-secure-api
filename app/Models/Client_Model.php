b51b217c9e0266f31a0b77c485afd5740e5f2ed4
El-Litios <carlosguzmanrojas00@gmail.com>
2022-03-24 23:00:44 -0300

    Agrega el modelo de clients

--- app/Models/Client_Model.php
+++ app/Models/Client_Model.php
@@ -1,8 +1,25 @@
 <?php
 
 namespace App\Models;
+use CodeIgniter\Model;
 
-class Client_Model
+class Client_Model extends Model
 {
+    protected $table = 'client';
+    protected $allowedFields = ['name', 'email', 'retainer_fee'];
+    protected $updatedField = 'updated_at';
     
+    /* protected $beforeInsert = ['beforeInsert'];
+    protected $beforeUpdate = ['beforeUpdate']; */
+
+    public function findClientById($id)
+    {
+        $client = $this->asArray()->Where(['id' => $id])->first();
+
+        if(!$client){
+            throw new \Exception('Client does not exist for specific id');
+        }
+
+        return $client;
+    }
 }
