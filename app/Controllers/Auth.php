c6b2a165c5180756ca2ad15035902555c7915863
El-Litios <carlosguzmanrojas00@gmail.com>
2022-03-29 22:51:30 -0300

    Se agrega archivo y logica de registro y logeo, junto a la logica de setear un token

---
+++ app/Controllers/Auth.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Auth extends BaseController
+{
+    public function register()
+    {
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|valid_email|is_unique[user.email]',
+            'password' => 'required|min_length[8]|max_length[255]'
+        ];
+
+        $input = $this->getRequestInput($this->request);
+
+        if(!$this->validateRequest($input, $rules))
+        {
+            return $this->getResponse($this->validator->getErrors(), ResponseInterface::HTTP_BAD_REQUEST);
+        }
+
+        $userModel = model('User_Model');
+        $userModel->save($input);
+
+        return $this->getJWTForUser($input['email'], ResponseInterface::HTTP_CREATED);
+    }
+
+    private function getJWTForUser(string $email, int $responseCode = ResponseInterface::HTTP_OK)
+    {
+        try {
+            $userModel = model('User_Model');
+            $user = $userModel->findUserByEmail($email); //verificar si el email corresponde a algun usuario dentro de la bd
+            unset($user['password']); //sacamos la pass del arreglo
+
+            helper('jwt');
+
+            return $this->getResponse([
+                'message' => 'Usuario authenticado!',
+                'user' => $user,
+                'access_token' => getSignedJWTForUser($email)
+            ]);
+        } catch (\Exception $e) {
+            return  $this->getResponse([
+                'error' => $e->getMessage()
+            ], $responseCode);
+        }
+    }
+
+
+    public function login()
+    {
+        $rules = [
+            'email' => 'required|valid_email|min_length[6]|max_length[60]',
+            'password' => 'required|min_length[8]|max_length[255]|validateUser[email, password]'
+        ];
+
+        $errors = [
+            'password' => [
+                'validateUser' => 'credenciales invalidas'
+            ]
+        ];
+
+        $input = $this->getRequestInput($this->request);
+
+        if(!$this->validateRequest($input, $rules, $errors))
+        {
+            return $this->getResponse($this->validator->getErrors(), ResponseInterface::HTTP_BAD_REQUEST);
+        }
+
+        return $this->getJWTForUser($input['email']);
+    }
+}
