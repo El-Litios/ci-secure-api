9826e98ddce30149984f36fd2448c270dd55a700
El-litios <carlosguzmanrojas00@gmail.com>
2022-05-31 14:13:17 -0400

    Modelo de categorias para los productois

---
+++ app/Models/CategoryProductsModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CategoryProductsModel extends Model
+{
+    protected $table            = 'categoryproduct';
+    protected $primaryKey       = 'id';
+
+}
