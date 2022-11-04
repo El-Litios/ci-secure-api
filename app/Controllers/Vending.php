33658d184a3a3e3eb1a992029e713099d7c02ed8
El-Litios <carlosguzmanrojas00@gmail.com>
2022-11-04 18:25:11 -0300

    Guardar venta

--- app/Controllers/Vending.php
+++ app/Controllers/Vending.php
@@ -3,9 +3,10 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\VendingModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
-class Product extends BaseController
+class Vending extends BaseController
 {
     public function index()
     {
@@ -17,6 +18,14 @@ class Product extends BaseController
 
     public function store()
     {
-        
+        $input = $this->getRequestInput($this->request);
+        $model = model('VendingModel');
+
+        $model->save($input);
+        $id = $model->insertID;
+
+        return $this->getResponse([
+            'message' => $id
+        ]);
     }
 }
